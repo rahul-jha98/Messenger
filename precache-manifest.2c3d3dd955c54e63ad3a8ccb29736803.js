68ca40d43ea1bb6d923e1f49073a6f7a415dccad
Rahul Jha <jharahul1998@gmail.com>
2020-06-12 16:15:22 +0530

    Updates

---
+++ precache-manifest.2c3d3dd955c54e63ad3a8ccb29736803.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589fed023b5ccde193c2ee61869151f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69e83188a59f8214fcce",
+    "url": "/static/css/main.d483c362.chunk.css"
+  },
+  {
+    "revision": "a7a52f47fc554e541d2b",
+    "url": "/static/js/2.8a0955a8.chunk.js"
+  },
+  {
+    "revision": "e65c219cda43bbd654c30f857ab81346",
+    "url": "/static/js/2.8a0955a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69e83188a59f8214fcce",
+    "url": "/static/js/main.edb1f22e.chunk.js"
+  },
+  {
+    "revision": "4d05d26280abc2e4013d",
+    "url": "/static/js/runtime-main.a71bade4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
